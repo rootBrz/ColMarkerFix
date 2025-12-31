9b06f8ceaf46f558add2122a18cb3bae7e31649b
rootBrz <alan2008358@gmail.com>
2025-12-31 15:24:09 +0500

    first commit

---
+++ ColMarkerFix/main.cpp
@@ -0,0 +1,40 @@
+﻿#include "shared/SafeWrite/SafeWrite.hpp"
+#include <nvse/PluginAPI.h>
+#include <shared/Defines.hpp>
+#include <shared/NVSEManager/NVSEGlobalManager.hpp>
+
+#define VERSION 100
+
+void MessageHandler(NVSEMessagingInterface::Message *msg)
+{
+        if (msg->type != NVSEMessagingInterface::kMessage_PostLoad)
+                return;
+
+        // NOP out the check that skips collision spheres, enabling culling for them
+        SafeWrite16(0x0056B4C2, 0x9090);
+}
+
+EXTERN_DLL_EXPORT bool NVSEPlugin_Query(const NVSEInterface *nvse, PluginInfo *info)
+{
+        info->infoVersion = PluginInfo::kInfoVersion;
+        info->name = "ColMarkerFix";
+        info->version = VERSION;
+        return true;
+}
+
+EXTERN_DLL_EXPORT bool NVSEPlugin_Load(NVSEInterface *nvse)
+{
+        if (!nvse->isEditor)
+        {
+                NVSEGlobalManager &rNVSE = NVSEGlobalManager::GetSingleton();
+                rNVSE.Initialize(nvse);
+                rNVSE.RegisterPluginEventListener("NVSE", MessageHandler);
+        }
+
+        return true;
+}
+
+BOOL WINAPI DllMain(HANDLE hDllHandle, DWORD dwReason, LPVOID lpreserved)
+{
+        return TRUE;
+}
---
+++ ColMarkerFix/xmake.lua
@@ -0,0 +1,29 @@
+set_project("ColMarkerFix")
+set_version("1.0")
+
+set_languages("cxx20")
+set_warnings("all")
+set_optimize("faster")
+set_symbols("debug")
+
+set_policy("build.optimization.lto", true)
+
+target("ColMarkerFix")
+	set_arch("x86")
+	set_kind("shared")
+	add_files("*.cpp")
+	add_files("../shared/NVSEManager/*.cpp")
+	add_files("../shared/SafeWrite/*.cpp")
+	add_includedirs("../")
+
+	add_linkdirs(".")
+
+	add_defines("WIN32", "_WINDOWS", "_USRDLL", "RUNTIME", "RUNTIME_VERSION=0x040020D0")
+
+	if is_mode("debug") then
+		add_defines("_DEBUG", "NVSE_PLUGIN_EXAMPLE_EXPORTS")
+		set_runtimes("MTd")
+	else
+		add_defines("NDEBUG")
+		set_runtimes("MD")
+	end
